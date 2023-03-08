8e29859ab736e295919ccbdf4c341206d4bca4d8
ProgrammerIosif <programmeriosif@protonmail.com>
2023-03-08 20:37:55 +0200

    implemented the data fetching from the weather api

--- app/page.js
+++ app/page.js
@@ -1,13 +1,63 @@
-import { Inter } from 'next/font/google'
+'use client';
 
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  const [route, setRoute] = useState();
+
+  async function getWeatherData (cityName,unit) {
+    const key = '701331af26f890a05517e29d2a156378';
+    //getting data from the openweathermap API
+    try {
+        const url1 = `https://api.openweathermap.org/data/2.5/forecast?units=${unit==='C' ? 'metric' : 'imperial'}&q=${cityName}&APPID=${key}`;
+        const data = await fetch(url1)
+            .then(function(response) {
+                    return response.json();
+                }).then(function(response) {return response;});
+        if(data.cod === '404')
+            throw new Error('City not found');
+        return data;
+    } catch (error) {
+        return error;
+    }
+  }
+  
+  const submitForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const city = form.elements[4].value;
+    form.elements[5].disabled = true;
+
+    let unit;
+    if(form.elements[2].checked)
+      unit = 'C';
+    else
+      unit = 'F';
+    let lang;
+
+    if(form.elements[0].checked)
+      lang = 'en';
+    else 
+      lang = 'ro';
+    getWeatherData(city,unit)
+      .then((data) => {
+        sessionStorage.setItem('data',JSON.stringify(data))
+        form.elements[5].disabled = false;
+        router.push("/" + city);
+        })
+      .catch((err) => {
+        form.elements[5].disabled = false;});
+  }
+  
   return (
     <main>
       <div className='flex flex-col items-center justify-around gap-10 h-[100vh] bg-base-100 text-neutral-content'>
         <div className="card w-96 bg-neutral text-neutral-content">
-          <div className="card-body">
+          <form className="card-body" onSubmit={submitForm}>
             <h1 className="text-5xl text-center font-bold mb-10">WEATHER</h1>
 
 
@@ -15,13 +65,13 @@ export default function Home() {
             <p className=' text-2xl'> LANGUAGE</p>
             <div className="form-control">
               <label className="label cursor-pointer">
-                <input type="radio" name="language" className="radio" checked />
+                <input type="radio" name="language" className="radio"/>
                 <span className="label-text text-2xl">English</span> 
               </label>
             </div>
             <div className="form-control">
               <label className="label cursor-pointer">
-                <input type="radio" name="language" className="radio" checked />
+                <input type="radio" name="language" className="radio"  />
                 <span className="label-text text-2xl">Romana</span> 
               </label>
             </div>
@@ -30,13 +80,13 @@ export default function Home() {
             <p className=' text-2xl'>TEMPERATURE IN</p>
             <div className="form-control">
               <label className="label cursor-pointer">
-                <input type="radio" name="unit" className="radio" checked />
+                <input type="radio" name="unit" className="radio"  />
                 <span className="label-text text-2xl">Celcius</span> 
               </label>
             </div>
             <div className="form-control">
               <label className="label cursor-pointer">
-                <input type="radio" name="unit" className="radio" checked />
+                <input type="radio" name="unit" className="radio"  />
                 <span className="label-text text-2xl">Fahrenheit</span> 
               </label>
             </div>
@@ -47,12 +97,12 @@ export default function Home() {
                 <span className="label-text text-2xl">LOCATION</span>
               </label>
               <div className='flex gap-4'>
-                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs bg-neutral" />
+                <input type="text" placeholder="" className="input input-bordered w-full max-w-xs bg-neutral" />
                 <button className="btn btn-outline">ENTER</button>
               </div>
             </div>
 
-          </div>
+          </form>
         </div>
       </div>
     </main>
